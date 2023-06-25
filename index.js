709f6e2b4a6affc0fca55fd56bd2d64444e9fb86
Tyler Moline <tmoline@devpipeline.com>
2023-06-25 15:26:20 -0600

    inital commit

---
+++ index.js
@@ -0,0 +1,31 @@
+randomize.addEventListener("click", randomHex);
+
+let newP = "";
+
+async function randomHex() {
+  try {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    document.body.style.backgroundColor = color;
+
+    if (newP === "") {
+      newP = document.createElement("p");
+      newP.textContent = `This color is: ${color}`;
+      newP.setAttribute("id", "hexCode");
+      theMainBoi.appendChild(newP);
+    } else {
+      theMainBoi.removeChild(newP);
+      newP = document.createElement("p");
+      newP.textContent = `This color is: ${color}`;
+      newP.setAttribute("id", "hexCode");
+      theMainBoi.appendChild(newP);
+    }
+    console.log(color);
+  } catch (error) {
+    console.log("error:", error);
+  }
+}
